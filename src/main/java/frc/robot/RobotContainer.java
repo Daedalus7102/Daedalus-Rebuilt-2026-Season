75528dc6e1c6c5dfd00c811afb48fedd59eeb146
ranch <mynameisjeffitowastaken@gmail.com>
2026-03-10 16:22:54 -0600

    hub pos but correct

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -24,7 +24,7 @@ public class RobotContainer {
 	);
 
 	// Example field point to aim at
-	private static final Translation2d kLookAtPoint = new Translation2d(8.27, 4.1);
+	private static final Translation2d kLookAtPoint = new Translation2d(4.62, 4.03);
 	private static final double kReducedDriveScale = 0.50;
 
 	// Autonomous
